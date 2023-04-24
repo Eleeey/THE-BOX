b689885d279500703f5860947a85addfb163773d
Eleeey <ezralaakasen@gmail.com>
2023-04-24 09:55:40 -0700

    mofied html and css

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+let btn = document.querySelector(".drop_dwn");
+let nav = document.querySelector(".links")
+btn.addEventListener('click',()=>{
+    console.log(nav.classList)
+    nav.classList.toggle("mobile_nav")
+})
